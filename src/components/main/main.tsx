cd4a73092af4f5088d0e10647de5a0614632aa65
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-03-25 01:09:55 -0300

    commit solidButton

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,6 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 import "../assets/background3.jpg";
 import { RootState } from "../../Redux/store";
 import { showModal } from "../../Redux/Slices/pagesSlice";
+import { Solidbutton } from "../utils/solidButton";
 
 
 
@@ -15,8 +16,13 @@ export function Main(){
 
     return(
 
-        <div className="h-full bg-red-400 text-white">
-            <button className="border" onClick={()=> dispath(showModal())}>abrir modal</button>
+        <div className="h-lvh bg-red-400 text-white">
+
+            <div>
+                <Solidbutton onClick={()=>console.log("Eduardo")} text="Video"></Solidbutton>
+                <Solidbutton onClick={()=>console.log("Eduardo")} text="Saiba Mais"></Solidbutton>
+            </div>
+
         </div>
 
     )
--- src/components/utils/solidButton.tsx
+++ src/components/utils/solidButton.tsx
@@ -1,13 +1,24 @@
+import { Id } from "@reduxjs/toolkit/dist/tsHelpers";
+import { text } from "stream/consumers";
+import './class.css';
+
+
+// Isso significa as propriedades que eu quero que meu button tenha
+interface propriedadesButton{
+    text: string;
+    onClick: Function;
+    className?: string;
+}
 
 
 
-export function Solidbutton(){
+export function Solidbutton(props:propriedadesButton){
 
     return(
         <div>
 
-            <button className="">
-                <h2>ola</h2>
+            <button className={"class" + props.className}>
+                <h2>{props.text}</h2>
             </button>
         
         </div>
