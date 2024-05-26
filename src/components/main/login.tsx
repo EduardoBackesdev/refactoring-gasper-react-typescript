e78212f78b433379d2a496c0b76bac5bd81cfb0e
Eduardo Backes <eduardo.programacao@hotmail.com>
2024-05-26 18:08:48 -0300

    Criação de pagina login-gasper

--- src/components/main/login.tsx
+++ src/components/main/login.tsx
@@ -1,16 +1,20 @@
-
+import '../Styles/login.css'
 
 export function Login (){
 
 
     return(
-        <div className="">
-            <h2>ola mundo</h2>
+        <div className="main-login">
             
+            <div className='border-login'>
+                <h2>Bem vindo ao ViewerGasper</h2>
 
+                <form className='formulario'>
+                    <input className='rounded-lg h-9' type="text" placeholder='Login' />
+                    <input className='rounded-lg h-9' type="password" placeholder='Senha'/>
+                </form>
 
-
-
+            </div>
         </div>
     )
 
